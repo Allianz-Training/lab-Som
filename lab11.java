951a532593a0a5aa199313ca423c035166ef1baa
ksillapapibul <ksillapapibul@gmail.com>
2020-11-23 15:00:51 +0700

    pair 23/11/2020

---
+++ lab11.java
@@ -0,0 +1,18 @@
+package com.buzzfreeze.helloWorld;
+
+public class lab11 {
+	public static void main(String[] args) {
+		a();
+		System.out.println(b());
+		
+	}
+	
+	static void a() {
+		System.out.print("hello ");
+	}
+	
+	static String b() {
+		return("world");
+	}
+	
+}
---
+++ lab8.java
@@ -0,0 +1,19 @@
+package com.buzzfreeze.helloWorld;
+
+public class lab8 {
+	public static void main(String[] args) {
+
+		int count = 1;
+
+		while (count <= 20) {
+			if (count == 11) {
+				System.out.println("This is "+count);
+				break;
+			}
+			System.out.println(count);
+			count++;
+		}
+
+	}
+
+}
