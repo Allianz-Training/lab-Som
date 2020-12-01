67cebabb9568b877179080f239cfc32bdd6d783b
ksillapapibul <51529529+ksillapapibul@users.noreply.github.com>
2020-12-02 02:18:07 +0700

    only array1

---
+++ Javascript/hwday2.js
@@ -0,0 +1,138 @@
+//1
+let array1 = [1,2,30,400]
+
+
+//2
+array1 = [1,2,3,4]
+
+
+//3
+array1 = [1, '1', 2, {}]
+
+//4
+array1 = ['apple', 'banana', 'orange']
+
+
+//5
+array1 = [
+    {name: 'apple', age: 14},
+    {name: 'banana', age: 18},
+    {name: 'watermelon', age: 32},
+]
+
+
+
+//6
+array1 = [
+    {name: 'apple', age: 14},
+    {name: 'banana', age: 18},
+    {name: 'watermelon', age: 32},
+]
+
+
+
+//7
+array1 = [
+    {name: 'apple', surname: 'London'},
+    {name: 'banana', surname: 'Bangkok'},
+    {name: 'watermelon', surname: 'Singapore'},
+]
+
+
+
+//8
+array1 = [1, 3, 4, 5, 6, 7, 8]
+
+
+//9
+array1 = [1, -3, 2, 8 ,-3, 5]
+
+
+//10
+array1 = [100, 200.25, 300.84, 400.3]
+
+
+//11
+array1 = [
+    {name: 'apple', birth: '2000-01-01'},
+    {name: 'banana', birth: '1990-10-01'},
+    {name: 'watermelon', birth: '1985-12-01'},
+]
+
+
+
+//12
+array1 = [
+    {name: 'apple', birth: '2000-01-01'},
+    {name: 'banana', birth: '1990-10-01'},
+    {name: 'watermelon', birth: '1985-12-01'},
+]
+
+//2.1
+array1 = [1,2,30,400]
+
+
+//2.2
+array1 = [1,2,3,4]
+
+
+//2.3
+array1 = [1,'1', 2, {}]
+
+
+//2.4
+array1 = ['apple', 'banana', 'orange', 'pineapple', 'watermelon']
+
+
+//2.5
+array1 = [
+    {name: 'apple', age: 14},
+    {name: 'banana', age: 18},
+    {name: 'watermelon', age: 32},
+    {name: 'pineapple', age: 16},
+    {name: 'peach', age: 24},
+]
+
+
+//2.6
+array1 = [
+    {name: 'apple', age: 14},
+    {name: 'banana', age: 18},
+    {name: 'watermelon', age: 32},
+    {name: 'pineapple', age: 16},
+    {name: 'peach', age: 24},
+]
+
+//2.7
+array1 = [1,-3,2,8,-4,5]
+
+
+//2.8
+array1 = [1,3,4,5,6,7,8]
+
+
+//2.9
+array1 = ['peach', 1, -3, '2', {}, []]
+
+
+//2.10
+array1 = ['APPLE', 'appLE', 'PEACH', 'PEach']
+
+
+//2.11
+array1 = [
+    {name: 'apple', birth: '2001-01-01'},
+    {name: 'banana', birth: '1990-10-10'},
+    {name: 'watermelon', birth: '1985-12-30'},
+    {name: 'peach', birth: '2002-10-13'},
+]
+
+
+
+//2.12
+array1 = [
+    {name: 'apple', birth: '2001-01-01'},
+    {name: 'banana', birth: '1990-10-10'},
+    {name: 'watermelon', birth: '1985-12-30'},
+    {name: 'peach', birth: '2002-10-13'},
+]
